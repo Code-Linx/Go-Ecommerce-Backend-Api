6b3ebce7ed06b158f82ba62b9cc4aeacc15c8a9c
Dennis <codetitan2206@gmail.com>
2025-05-07 18:15:29 +0100

    feat: set up initial project structure with basic API server and user routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/Code-Linx/Go-Ecommerce-Backend-Api/cmd/api"
+)
+
+func main() {
+	server := api.NewAPIServer(":8080", nil)
+	server.Run()
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Code-Linx/Go-Ecommerce-Backend-Api
 
 go 1.24.0
+
+require github.com/gorilla/mux v1.8.1
