6f98715432bb5b153bb2e60bb1865a33a5067001
Dennis <codetitan2206@gmail.com>
2025-05-08 07:25:31 +0100

    feat: add environment config loader and initialize MySQL storage connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/Code-Linx/Go-Ecommerce-Backend-Api/cmd/api"
+	"github.com/Code-Linx/Go-Ecommerce-Backend-Api/config"
+	"github.com/Code-Linx/Go-Ecommerce-Backend-Api/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	InitStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 	server.Run()
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func InitStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB Sucessfully Connceted!")
 }
