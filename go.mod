607c8bab88e066253def989a7d9351c27a18265a
Dennis <codetitan2206@gmail.com>
2025-05-17 09:45:55 +0100

    feat(user): add payload validation and success test for user registration

--- go.mod
+++ go.mod
@@ -7,6 +7,17 @@ require github.com/gorilla/mux v1.8.1
 require golang.org/x/crypto v0.38.0
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.26.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.2
 	github.com/joho/godotenv v1.5.1
--- service/user/routes_test.go
+++ service/user/routes_test.go
@@ -20,12 +20,12 @@ func TestUserServiceHandlers(t *testing.T) {
 		payload := types.RegisterUserPayload{
 			FirstName: "user",
 			LastName:  "name",
-			Email:     "",
-			Password:  "123",
+			Email:     "asd",
+			Password:  "asd",
 		}
 
 		marshalled, _ := json.Marshal(payload)
-		req, err := http.NewRequest(http.MethodPost, "register", bytes.NewBufferString(string(marshalled)))
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -35,6 +35,28 @@ func TestUserServiceHandlers(t *testing.T) {
 		router.ServeHTTP(rr, req)
 		if rr.Code != http.StatusBadRequest {
 			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, rr.Code)
+		}
+	})
+
+	t.Run("should correctly register the user", func(t *testing.T) {
+		payload := types.RegisterUserPayload{
+			FirstName: "user",
+			LastName:  "name",
+			Email:     "asd@gmail.com",
+			Password:  "asd",
+		}
+
+		marshalled, _ := json.Marshal(payload)
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+		router.HandleFunc("/register", handler.handleRegister)
+		router.ServeHTTP(rr, req)
+		if rr.Code != http.StatusCreated {
+			t.Errorf("expected status code %d, got %d", http.StatusCreated, rr.Code)
 		}
 	})
 }
